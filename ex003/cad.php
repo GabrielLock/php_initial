3b2bc80490c7392a016cdfdf6f62a5240c1833df
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-04 07:51:20 -0300

    learning forms

---
+++ ex003/cad.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Resultado</title>
+</head>
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <?php 
+            $nome = $_GET["nome"] ?? "sem nome";
+            $snome = $_GET["snome"] ?? "desconhecido";
+            echo "<p>É um prazer te conhecer, <strong>$nome $snome</strong>! Este é o meu site. ";
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </main>
+</body>
+</html>
