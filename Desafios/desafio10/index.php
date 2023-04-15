0666fe8010da6d54a9fb0d21ab52f3bb2fc87012
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-15 07:36:12 -0300

    preparing the files for the next challenges creating the interfaces

---
+++ Desafios/desafio10/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cálculo de Idade</title>
+</head>
+<body>
+    <main>
+        <h1>Calculando a sua idade</h1>
+        <form action="" method="get">
+            <label for="age">Em que ano você nasceu?</label>
+            <input type="number" name="age" id="age">
+            <label for="agef">Quer saber sua idade em que ano?(atualmente estamos em)</label>
+            <input type="number" name="agef" id="agef">
+            <input type="submit" value="Qual será minha idade?">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado</h2>
+    </section>
+</body>
+</html>
---
+++ Desafios/desafio9/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Médias Aritméticas</title>
+</head>
+<body>
+    <main>
+        <h1>Médias Aritméticas</h1>
+        <form action="" method="get">
+            <label for="v1">1° Valor</label>
+            <input type="number" name="v1" id="v1">
+            <label for="p1">1° Peso</label>
+            <input type="number" name="p1" id="p1">
+            <label for="v2">2° Valor</label>
+            <input type="number" name="v2" id="v2">
+            <label for="p2">2° Peso</label>
+            <input type="number" name="p2" id="p2">
+            <input type="submit" value="Calcular Médias">
+        </form>
+    </main>
+    <section>
+        <h2>Cálculo das Médias</h2>
+    </section>
+</body>
+</html>
