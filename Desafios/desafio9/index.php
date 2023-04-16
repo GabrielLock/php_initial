8ad9947ede034af41904f0cb68b3842aa29a8a50
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-16 12:25:24 -0300

    Challenge 009: arithmetic averages

--- Desafios/desafio9/index.php
+++ Desafios/desafio9/index.php
@@ -8,6 +8,12 @@
     <title>Médias Aritméticas</title>
 </head>
 <body>
+    <?php 
+        $v1 = $_GET['v1'] ?? 1;
+        $v2 = $_GET['v2'] ?? 1;
+        $p1 = $_GET['p1'] ?? 1;
+        $p2 = $_GET['p2'] ?? 1;
+    ?>
     <main>
         <h1>Médias Aritméticas</h1>
         <form action="" method="get">
@@ -24,6 +30,14 @@
     </main>
     <section>
         <h2>Cálculo das Médias</h2>
+        <p><?="Analisando os valores $v1 e $v2:"?></p>
+        <?php 
+            $mediaSimples = ($v1 + $v2) / 2;
+            $mediaPonderada = ($v1 * $p1 + $v2 * $p2) / ($p1 + $p2);
+            echo "<ul>";
+            echo "<li>A <strong>Média Aritmétrica Simples</strong> entre os valores é igual a ". number_format($mediaSimples, 2, ",", ".") .".</li>";
+            echo "<li>A <strong>Média Aritmétrica Ponderada</strong> com pesos $p1 e $p2 é igual a ". number_format($mediaPonderada, 2, ",", ".") .".</li>";
+            echo "</ul>"?>
     </section>
 </body>
 </html>
