68e032a57e119d68e547fe427e9414f80e815310
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-17 07:28:04 -0300

    Challenge 010 : Age calculation needs some adjustments

--- Desafios/desafio10/index.php
+++ Desafios/desafio10/index.php
@@ -8,18 +8,25 @@
     <title>Cálculo de Idade</title>
 </head>
 <body>
+    <?php 
+    $nasceu = $_GET['age'] ?? 1985;
+    $ano = $_GET['agef'] ?? $anoAtual;
+    $anoAtual = date("Y");
+    $idade = $ano - $nasceu;
+    ?>
     <main>
         <h1>Calculando a sua idade</h1>
         <form action="" method="get">
             <label for="age">Em que ano você nasceu?</label>
             <input type="number" name="age" id="age">
-            <label for="agef">Quer saber sua idade em que ano?(atualmente estamos em)</label>
+            <label for="agef">Quer saber sua idade em que ano?(atualmente estamos em <strong>2023</strong>)</label>
             <input type="number" name="agef" id="agef">
             <input type="submit" value="Qual será minha idade?">
         </form>
     </main>
     <section>
         <h2>Resultado</h2>
+        <p><?="Quem nasceu em $nasceu vai ter <strong>$idade anos</strong> em $ano!"?></p>
     </section>
 </body>
 </html>
