e87fab2f6928b7ec44b56a57354662b724c5012c
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-20 13:37:56 -0300

    Challenge 012: Time Calculator

--- Desafios/desafio12/index.php
+++ Desafios/desafio12/index.php
@@ -8,16 +8,38 @@
     <title>Calculadora de Tempo</title>
 </head>
 <body>
+    <?php 
+        $segundos = $_GET['seg'] ?? 0;
+    ?>
     <main>
         <h1>Calculadora de Tempo</h1>
-        <form action="" method="get">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="seg">Qual é o total de segundos?</label>
-            <input type="number" name="seg" id="seg">
+            <input type="number" name="seg" id="seg" value="<?=$segundos?>">
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <h2>Totalizando tudo</h2>
+        <p><?="Analisando o valor que você digitou, <strong>" . number_format($segundos, 0, ".", ".") . " segundos</strong> equivalem a um total de:"?></p>
+        <?php 
+        $semanas = intdiv($segundos, 604_800);
+        $difSemanas = $segundos % 604_800;
+        $dias = intdiv($difSemanas, 86_400);
+        $difDias = $difSemanas % 86_400;
+        $horas = intdiv($difDias, 3_600);
+        $difHoras = $difDias % 3_600;
+        $minutos = intdiv($difHoras, 60);
+        $segundos = $difHoras % 60;
+
+            echo "<ul>";
+            echo "<li>$semanas semanas</li>";
+            echo "<li>$dias dias</li>";
+            echo "<li>$horas horas</li>";
+            echo "<li>$minutos minutos</li>";
+            echo "<li>$segundos segundos</li>";
+            echo "</ul>"
+        ?>
     </section>
 </body>
 </html>
