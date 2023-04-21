3543760ac89dd3a41179ea81bc39315419f62782
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-04-21 17:30:13 -0300

    adding the images for challenge 13

--- Desafios/desafio13/index.php
+++ Desafios/desafio13/index.php
@@ -18,6 +18,15 @@
     </main>
     <section>
         <h2>Saque de realizado</h2>
+        <p><?="O caixa eletrônico vai te entregar as seguintes notas:"?></p>
+        <?php 
+            echo "<ul>";
+            echo "<li><img src=\"./realNotas/100-reais.jpg\" alt=\"nota de cem reais\"> x</li>";
+            echo "<li><img src=\"./realNotas/50-reais.jpg\" alt=\"nota de cinquenta reais\"> x</li>";
+            echo "<li><img src=\"./realNotas/10-reais.jpg\" alt=\"nota de dez reais\"> x</li>";
+            echo "<li><img src=\"./realNotas/5-reais.jpg\" alt=\"nota de cinco reais\"> x</li>";
+            echo "</ul>"
+        ?>
     </section>
 </body>
 </html>
